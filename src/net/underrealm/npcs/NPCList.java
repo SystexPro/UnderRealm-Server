a3e5dd9c90ccfbbfe61746ce15107484777fb6e2
Christian Horton <christian@tweakuniverse.com>
2011-05-01 11:28:51 -0700

    Added 2 NPC's

--- src/net/underrealm/npcs/NPCList.java
+++ src/net/underrealm/npcs/NPCList.java
@@ -3,10 +3,12 @@ package net.underrealm.npcs;
 public class NPCList {
 
 	public static NPC Bob = new NPC();
+	public static NPC Wiggum = new NPC();
 
 
 	static {
 		Bob.setNPCName("Bob").setNPCHealth(10).setNPCGender("male");
+		Wiggum.setNPCName("ChiefWiggum").setNPCHealth(10).setNPCGender("female");
 
 	}
 }
