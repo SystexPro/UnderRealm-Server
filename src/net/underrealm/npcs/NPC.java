fa489f66688238e8762bbe83f1a4abc761b51d0d
Christian Horton <christian@tweakuniverse.com>
2011-05-01 11:26:20 -0700

    Added One NPC

--- src/net/underrealm/npcs/NPC.java
+++ src/net/underrealm/npcs/NPC.java
@@ -1,5 +1,29 @@
 package net.underrealm.npcs;
 
 public class NPC {
-
+	
+	
+	
+	public NPC() {
+		
+	}
+	
+	public NPC setNPCHealth(int hea) {
+		health = hea;
+		return this;
+	}
+	
+	public NPC setNPCName(String name) {
+		npcName = name;
+		return this;
+	}
+	
+	public NPC setNPCGender(String gen) {
+		gender = gen;
+		return this;
+	}
+	
+	public int health;
+	public String npcName;
+	public String gender;
 }
---
+++ src/net/underrealm/npcs/NPCList.java
@@ -0,0 +1,12 @@
+package net.underrealm.npcs;
+
+public class NPCList {
+
+	public static NPC Bob = new NPC();
+
+
+	static {
+		Bob.setNPCName("Bob").setNPCHealth(10).setNPCGender("male");
+
+	}
+}
