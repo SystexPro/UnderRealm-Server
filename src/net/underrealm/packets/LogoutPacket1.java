70bfafb8a0c53465a8eedf0a0e8630f130efc281
Christian Horton <christian@tweakuniverse.com>
2011-05-05 16:03:42 -0700

    Added Logout Packet

---
+++ src/net/underrealm/packets/LogoutPacket1.java
@@ -0,0 +1,15 @@
+package net.underrealm.packets;
+
+public class LogoutPacket1 extends PacketHandler {
+
+	public LogoutPacket1(Class packetClass, int i) {
+		super(packetClass, i);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static PacketHandler handler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/net/underrealm/packets/PacketHandler.java
+++ src/net/underrealm/packets/PacketHandler.java
@@ -11,11 +11,15 @@ public class PacketHandler {
 	public PacketHandler handlePacket(Class pclass) {
 		if(pclass.equals(LoginPacket0.class)) {
 			return LoginPacket0.handler();
-		} return null;
+		} else if (pclass.equals(LogoutPacket1.class)) {
+			return LogoutPacket1.handler();
+		}
+		return null;
 	}
 	
 	static {
 		PacketHandler LoginPacket0 = new PacketHandler(LoginPacket0.class, 0);
+		PacketHandler LogoutPacket1 = new PacketHandler(LogoutPacket1.class, 1);
 	}
 
 
