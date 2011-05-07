0276002d7cfc72c839fe3d2de6bb1c807bf61c45
Christian Horton <christian@tweakuniverse.com>
2011-05-06 20:02:06 -0700

    Added Another Item and Items.Type enum

--- src/net/underrealm/items/Items.java
+++ src/net/underrealm/items/Items.java
@@ -1,17 +1,58 @@
 package net.underrealm.items;
 
 public class Items {
-
 	
-	public Items setName(String name) {
+	/**
+	 * Set Item Name
+	 * @param name
+	 * @return
+	 */
+	protected Items setName(String name) {
 		name = itemName;
 		return this;
 	}
 	
-	public String itemName;
-	public static Items Magic_Wand;
+	/**
+	 * Set Item Id
+	 * @param id
+	 * @return
+	 */
+	protected Items setItemId(int id) {
+		itemId = id;
+		return this;
+	}
 	
+	/**
+	 * Set Item Type
+	 * @param type
+	 * @return
+	 */
+	protected Items setType(Items.Type type) {
+		this.itemType = type;
+		return this;
+	}
+	
+	/**
+	 * Type of Item
+	 * @author SystexPro
+	 *
+	 */
+	public enum Type {
+		ARMOR,
+		WEAPON,
+		MAGIC,
+		NULL;
+	}
+
+	public int itemId;
+	public String itemName;
+	public Items.Type itemType;
+	//Items//
+	public static Items Magic_Wand;
+	public static Items Sythe_Wand;
+	public static Items Leather_Boots;
 	static {
-		Magic_Wand.setName("magic_wand");
+		Magic_Wand.setItemId(0).setName("item.magic_wand").setType(Items.Type.MAGIC);
+		Sythe_Wand.setItemId(1).setName("item.sythewand").setType(Items.Type.MAGIC);
 	}
 }
